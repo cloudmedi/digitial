6b69d1cacf3663621ab5869bcd65f6a87c7b45bd
M <blackbunny@gmail.com>
2024-05-06 19:39:21 +0300

    widget module base done

---
+++ services/widget/module/image.service.js
@@ -0,0 +1,150 @@
+"use strict";
+
+const {MoleculerClientError} = require("moleculer").Errors;
+const {ForbiddenError} = require("moleculer-web").Errors;
+const DbMixin = require("../../../mixins/db.mixin");
+const {ObjectId} = require("mongodb");
+
+/**
+ * @typedef {import("moleculer").Context} Context Moleculer's Context
+ */
+
+module.exports = {
+	name: "widget.image",
+	version: 1,
+
+	/**
+	 * Mixins
+	 */
+	mixins: [DbMixin("widget_image")],
+	whitelist: [],
+	/**
+	 * Settings
+	 */
+	settings: {
+		// Available fields in the responses
+		fields: [
+			"_id",
+			"name",
+			"provider",
+			"slug",
+			"path",
+			"domain",
+		],
+
+		// Validator for the `create` & `insert` actions.
+		entityValidator: {
+			name: "string",
+			slug: "string"
+		},
+		populates: {}
+	},
+
+	/**
+	 * Action Hooks
+	 */
+	hooks: {
+		before: {
+			/**
+			 * Register a before hook for the `create` action.
+			 * It sets a default value for the quantity field.
+			 *
+			 * @param {Context} ctx
+			 */
+			create(ctx) {
+				ctx.params.createddAt = new Date();
+				ctx.params.updatedAt = null;
+				ctx.params.user = new ObjectId(ctx.meta.user.id);
+				ctx.params.status = true;
+			},
+			update(ctx) {
+				ctx.params.updatedAt = new Date();
+			}
+		}
+	},
+
+	/**
+	 * Actions
+	 */
+	actions: {
+		properties: {
+			rest: "GET /properties",
+			async handler(ctx) {
+				return {
+					name: "image",
+					params: ["path", "domain", "place", "width", "height"],
+					status: true
+				};
+			}
+		},
+		create: false,
+		list: false,
+		get: false,
+		count: false,
+		insert: false,
+		update: false,
+		remove: false
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+		/**
+		 * Find an wallet by user
+		 *
+		 * @param {String} user - Article slug
+		 *
+		 * @results {Object} Promise<Article
+		 */
+		findByUser(user) {
+			return this.adapter.findOne({user});
+		},
+
+		/**
+		 * Transform the result entities to follow the API spec
+		 *
+		 * @param {Context} ctx
+		 * @param {Array} entities
+		 * @param {Object} user - Logged in user
+		 */
+		async transformResult(ctx, entities, user) {
+			if (Array.isArray(entities)) {
+				const currency = await this.Promise.all(entities.map(item => this.transformEntity(ctx, item, user)));
+				return {
+					currency
+				};
+			} else {
+				const currency = await this.transformEntity(ctx, entities);
+				return {currency};
+			}
+		},
+
+		/**
+		 * Transform a result entity to follow the API spec
+		 *
+		 * @param {Context} ctx
+		 * @param {Object} entity
+		 * @param {Object} user - Logged in user
+		 */
+		async transformEntity(ctx, entity) {
+			if (!entity) return null;
+
+			return entity;
+		},
+		/**
+		 * Loading sample data to the collection.
+		 * It is called in the DB.mixin after the database
+		 * connection establishing & the collection is empty.
+		 */
+		async seedDB() {
+		},
+	},
+
+	/**
+	 * Fired after database connection establishing.
+	 */
+	async afterConnected() {
+		// await this.adapter.collection.createIndex({ name: 1 });
+	}
+};
