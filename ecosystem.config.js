0f8c8202738e2a0ac00359d11247ce8b16babcc2
M <blackbunny@gmail.com>
2024-06-05 12:04:30 +0300

    fix cache screen

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	apps: [
 		{
-			name: "signage-apibase",
+			name: "apibase",
 			script: "./node_modules/.bin/moleculer-runner",
 			args: "services/api.service.js services/io.service.js services/room.service.js services/lab.service.js services/profile.service.js services/room.service.js services/users.service.js services/wallet.service.js",
 			exec_mode: "cluster",
@@ -9,21 +9,21 @@ module.exports = {
 			env: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-apibase"
+				NODE_ID: "apibase"
 			},
 			env_production: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-apibase"
+				NODE_ID: "apibase"
 			},
 			env_development: {
 				NODE_ENV: "development",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-dev-apibase"
+				NODE_ID: "dev-apibase"
 			}
 		},
 		{
-			name: "signage-partials",
+			name: "partials",
 			script: "./node_modules/.bin/moleculer-runner",
 			args: "services/_partial/currency.service.js services/_partial/ip_location.service.js",
 			exec_mode: "cluster",
@@ -31,21 +31,21 @@ module.exports = {
 			env: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-partials"
+				NODE_ID: "partials"
 			},
 			env_production: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-partials"
+				NODE_ID: "partials"
 			},
 			env_development: {
 				NODE_ENV: "development",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-dev-partials"
+				NODE_ID: "dev-partials"
 			}
 		},
 		{
-			name: "signage-admin",
+			name: "admin",
 			script: "./node_modules/.bin/moleculer-runner",
 			args: "services/admin/country.service.js",
 			exec_mode: "cluster",
@@ -53,21 +53,21 @@ module.exports = {
 			env: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-admin"
+				NODE_ID: "admin"
 			},
 			env_production: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-admin"
+				NODE_ID: "admin"
 			},
 			env_development: {
 				NODE_ENV: "development",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-dev-admin"
+				NODE_ID: "dev-admin"
 			}
 		},
 		{
-			name: "signage-screens",
+			name: "screens",
 			script: "./node_modules/.bin/moleculer-runner",
 			args: "services/screen/screen.service.js services/screen/device.service.js",
 			exec_mode: "cluster",
@@ -75,21 +75,21 @@ module.exports = {
 			env: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-screens"
+				NODE_ID: "screens"
 			},
 			env_production: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-screens"
+				NODE_ID: "screens"
 			},
 			env_development: {
 				NODE_ENV: "development",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-dev-screens"
+				NODE_ID: "dev-screens"
 			}
 		},
 		{
-			name: "signage-package",
+			name: "package",
 			script: "./node_modules/.bin/moleculer-runner",
 			args: "services/package/packages.service.js",
 			exec_mode: "cluster",
@@ -97,21 +97,21 @@ module.exports = {
 			env: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-package"
+				NODE_ID: "package"
 			},
 			env_production: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-package"
+				NODE_ID: "package"
 			},
 			env_development: {
 				NODE_ENV: "development",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-dev-package"
+				NODE_ID: "dev-package"
 			}
 		},
 		{
-			name: "signage-email",
+			name: "email",
 			script: "./node_modules/.bin/moleculer-runner",
 			args: "services/email/email.service.js",
 			exec_mode: "cluster",
@@ -119,21 +119,21 @@ module.exports = {
 			env: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-email"
+				NODE_ID: "email"
 			},
 			env_production: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-email"
+				NODE_ID: "email"
 			},
 			env_development: {
 				NODE_ENV: "development",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-dev-email"
+				NODE_ID: "dev-email"
 			}
 		},
 		{
-			name: "signage-source",
+			name: "source",
 			script: "./node_modules/.bin/moleculer-runner",
 			args: "services/source/source.service.js services/source/layout.service.js services/source/channel.service.js services/source/playlist.service.js services/source/program.service.js",
 			exec_mode: "cluster",
@@ -141,21 +141,21 @@ module.exports = {
 			env: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-source"
+				NODE_ID: "source"
 			},
 			env_production: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-source"
+				NODE_ID: "source"
 			},
 			env_development: {
 				NODE_ENV: "development",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-dev-source"
+				NODE_ID: "dev-source"
 			}
 		},
 		{
-			name: "signage-widget",
+			name: "widget",
 			script: "./node_modules/.bin/moleculer-runner",
 			args: "services/widget/widget.service.js services/widget/filemanager.service.js services/widget/module/image.service.js services/widget/module/video.service.js",
 			exec_mode: "cluster",
@@ -163,17 +163,17 @@ module.exports = {
 			env: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-widget"
+				NODE_ID: "widget"
 			},
 			env_production: {
 				NODE_ENV: "production",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-widget"
+				NODE_ID: "widget"
 			},
 			env_development: {
 				NODE_ENV: "development",
 				LAB_PORT: 3211,
-				NODE_ID: "signage-dev-widget"
+				NODE_ID: "dev-widget"
 			}
 		}
 	]
